e7f74fc53b59ecb9ef1d8deff328b4eec4e53e6e
ahmedalam98 <118381216+ahmedalam98@users.noreply.github.com>
2022-12-30 18:18:31 +0200

    feat: :sparkles: Get search result query

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,11 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 
 // Polyfilling
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-const recipeContainer = document.querySelector('.recipe');
-
-///////////////////////////////////////
 // 1.1 async function calling another async function
 const controlRecipes = async function () {
   try {
@@ -27,7 +25,24 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
