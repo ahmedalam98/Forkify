3154bae41c9df89f1d5d63982892f308b95eeeb8
ahmedalam98 <118381216+ahmedalam98@users.noreply.github.com>
2022-12-31 12:57:10 +0200

    refactor: :recycle: Refactor search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,10 +1,15 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 // Polyfilling
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 // 1.1 async function calling another async function
 const controlRecipes = async function () {
@@ -27,6 +32,8 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -35,7 +42,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
-    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
