efbecab250c5377bfa8fd45eb94faeb690a68f55
ahmedalam98 <118381216+ahmedalam98@users.noreply.github.com>
2023-01-03 17:20:08 +0200

    Continue uploading a new recipe

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '182c8c83-d49c-44f9-8038-d3f16ed5aee8';
+export const MODAL_CLOSE_SEC = 2.5;
